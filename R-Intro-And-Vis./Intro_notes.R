c59533062b1bf443ee1489f35d2cdf9e57be5381
Apurva <apurvasingh27@gmail.com>
2025-06-14 07:55:05 +0000

    Completed notes for Intro to R lecture

--- R-Intro-And-Vis./Intro_notes.R
+++ R-Intro-And-Vis./Intro_notes.R
@@ -69,4 +69,60 @@ shopping_df[c(2,4), c("Amount","Price")] #Rows 2 and 4 for given columns
 shopping_df$Total <- shopping_df$Amount * shopping_df$Price
 sum(shopping_df$Total) #Adds up all the values from the total column.
 
-#LOOPS
+#LOOPS AND CONDITIONAL
+Sample_list <- c("Alfredo", "Pasta", "Water", "Olive oil")
+for (i in Sample_list){
+   cat("Make sure to purchase: ", i, "\n")
+   if (i == "Pasta"){
+    cat("Please purchase penne-style pasta.\n")
+   }
+}
+
+#Taking user input
+your_name <- readline(prompt = "Enter your name: ")
+cat("Hi ", your_name, "!\n", sep="") #Last argument removes empty spaces across concat. strings.
+
+#Functions
+Egg.boiling <- function(){
+    cat("Place your eggs in a pot full of cold water.\n")
+    cat("Place the pot on a stovetop and raise the temperature to medium.\n")
+    cat("Once the water starts boiling, drop the temperature to medium-low.\n")
+    cat("After 10-12minutes, remove the eggs from the water and place them in cold water again.\n")
+    cat("Peel the boiled eggs and use as per your recipe.")
+}
+
+#Arguments
+Salad.steps <- function(item1, item2, item3){
+    cat("Add ", item1, "\n", sep="")
+    cat("Add ", item2, "\n", sep="")
+    cat("Add ", item3, "\n", sep="")
+    cat("Mix everything well. \n")
+}
+
+Salad.steps("Lettuce", "Tomato", "Vinegar") #The arguments are the input to the function.
+
+#Variable number of parameters
+mix.ingredients <- function(ingredients){
+    for (i in ingredients)
+        cat("Add", i, "\n")
+    cat("Mix everything well\n")
+}
+
+ingredients <- c("sugar", "flour", "raisins", "vanilla")
+mix.ingredients(ingredients)
+
+#Default argument, return statement, and "paste"
+make.pasta.bolognese <- function(sauce.ingredients, pasta="Spaghetti"){
+    #Paste simply concatenates its inputs into a single string.
+    #Collapse separates the concatenated strings with the given symbol.
+    ingredients.string <- paste(sauce.ingredients, collapse=", ") 
+    cat("Cook", ingredients.string, "together\n")
+    cat("Serve on top of", pasta, "\n")
+    return(paste("Bolognese sauce with", pasta))
+    #Return statement allows us to use the output of the function later.
+}
+
+make.pasta.bolognese(c("onions", "ground beef", "tomatoes"))
+#Despite not declaring the pasta argument in this function, the DEFAULT spagetthi is used as argument for the function.
+
+##END OF LECTURE - HIGH-PERFORMANCE R NOT COVERED. =)
